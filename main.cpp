53469447abd7c16548362b33a8f4d59bfb064826
Benoit Gloria <gloria_b@epita.fr>
2023-04-10 22:45:22 +0200

    simple echo server

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <memory>
+#include <uvw.hpp>
+
+void listen(uvw::loop& loop)
+{
+    std::shared_ptr<uvw::tcp_handle> tcp = loop.resource<uvw::tcp_handle>();
+
+    tcp->on<uvw::listen_event>([](const uvw::listen_event&,
+                                   uvw::tcp_handle& srv) {
+        std::shared_ptr<uvw::tcp_handle> client = srv.parent().resource<uvw::tcp_handle>();
+
+        client->on<uvw::end_event>([](const uvw::end_event&,
+                                       uvw::tcp_handle& client) { client.close(); });
+        client->on<uvw::data_event>(
+            [](uvw::data_event& data, uvw::tcp_handle& client) {
+                client.write(std::move(data.data), data.length);
+            });
+
+        srv.accept(*client);
+        client->read();
+    });
+
+    tcp->bind("127.0.0.1", 4242);
+    tcp->listen();
+}
+
+int main()
+{
+    auto loop = uvw::loop::get_default();
+    listen(*loop);
+    loop->run();
+}
